4cfe8597c4caaf344b81e4dfce9bd05941e545d0
r-knm <kainuma@log-build.com>
2024-05-18 11:14:49 +0900

    コントローラクラスを実装

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TenantEntity } from './entities/tenant.entity';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('tenants')
+  async createTenant(): Promise<TenantEntity> {
+    return await this.appService.createTenant();
+  }
+
+  @Post('users')
+  async createUser(): Promise<UserEntity> {
+    return await this.appService.createUser();
   }
 }
