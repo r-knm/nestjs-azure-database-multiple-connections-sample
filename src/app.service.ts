66f45c9769340db3364b9f1620872b4975652b7f
r-knm <kainuma@log-build.com>
2024-05-18 11:13:24 +0900

    サービスクラスを実装

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,46 @@
+import { InjectModel } from '@nestjs/azure-database';
 import { Injectable } from '@nestjs/common';
+import { Container } from '@azure/cosmos';
+import { v4 as uuidv4 } from 'uuid';
+import { TenantEntity } from './entities/tenant.entity';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectModel(TenantEntity)
+    protected readonly tenantsContainer: Container,
+
+    @InjectModel(UserEntity)
+    protected readonly usersContainer: Container,
+  ) {}
+
+  async createTenant(): Promise<TenantEntity> {
+    const now = new Date();
+    const newTenant = {
+      id: uuidv4(),
+      name: 'Acme Corp',
+      createdAt: now,
+      updatedAt: now,
+    };
+
+    const { resource: tenantItem } =
+      await this.tenantsContainer.items.create(newTenant);
+
+    return tenantItem;
+  }
+
+  async createUser(): Promise<UserEntity> {
+    const now = new Date();
+    const user = {
+      id: uuidv4(),
+      name: 'John Doe',
+      createdAt: now,
+      updatedAt: now,
+    };
+
+    const { resource: userItem } = await this.usersContainer.items.create(user);
+
+    return userItem;
   }
 }
